ed74d82810e92460a0b56610d4d58e000173258d
dperales2022 <111503354+dperales2022@users.noreply.github.com>
2024-04-26 18:48:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -78,6 +78,125 @@ async def process_pdf(request: ProcessRequest):
         base_nodes, objects = node_parser.get_nodes_and_objects(nodes)
 
         initialiseNeo4jSchema()
+
+        driver = GraphDatabase.driver(NEO4J_URL, database=NEO4J_DATABASE, auth=(NEO4J_USER, NEO4J_PASSWORD))
+        
+        print("Start saving documents to Neo4j...")
+        i = 0
+        with driver.session() as session:
+            for doc in documents:
+                cypher = "MERGE (d:Document {url_hash: $doc_id}) ON CREATE SET d.url=$url;"
+                session.run(cypher, doc_id=doc.doc_id, url=doc.doc_id)
+                i = i + 1
+            session.close()
+        
+        print(f"{i} documents saved.")
+        
+        print("Start saving nodes to Neo4j...")
+        
+        i = 0
+        with driver.session() as session:
+            for node in base_nodes: 
+        
+                # >>1 Create Section node
+                cypher  = "MERGE (c:Section {key: $node_id})\n"
+                cypher += " FOREACH (ignoreMe IN CASE WHEN c.type IS NULL THEN [1] ELSE [] END |\n"
+                cypher += "     SET c.hash = $hash, c.text=$content, c.type=$type, c.class=$class_name, c.start_idx=$start_idx, c.end_idx=$end_idx )\n"
+                cypher += " WITH c\n"
+                cypher += " MATCH (d:Document {url_hash: $doc_id})\n"
+                cypher += " MERGE (d)<-[:HAS_DOCUMENT]-(c);"
+        
+                node_json = json.loads(node.json())
+        
+                session.run(cypher, node_id=node.node_id, hash=node.hash, content=node.get_content(), type='TEXT', class_name=node.class_name()
+                                  , start_idx=node_json['start_char_idx'], end_idx=node_json['end_char_idx'], doc_id=node.ref_doc_id)
+        
+                # >>2 Link node using NEXT relationship
+        
+                if node.next_node is not None: # and node.next_node.node_id[-1*len(TABLE_REF_SUFFIX):] != TABLE_REF_SUFFIX:
+                    cypher  = "MATCH (c:Section {key: $node_id})\n"    # current node should exist
+                    cypher += "MERGE (p:Section {key: $next_id})\n"    # previous node may not exist
+                    cypher += "MERGE (p)<-[:NEXT]-(c);"
+        
+                    session.run(cypher, node_id=node.node_id, next_id=node.next_node.node_id)
+        
+                if node.prev_node is not None:  # Because tables are in objects list, so we need to link from the opposite direction
+                    cypher  = "MATCH (c:Section {key: $node_id})\n"    # current node should exist
+                    cypher += "MERGE (p:Section {key: $prev_id})\n"    # previous node may not exist
+                    cypher += "MERGE (p)-[:NEXT]->(c);"
+        
+                    if node.prev_node.node_id[-1 * len(TABLE_ID_SUFFIX):] == TABLE_ID_SUFFIX:
+                        prev_id = node.prev_node.node_id + '_ref'
+                    else:
+                        prev_id = node.prev_node.node_id
+        
+                    session.run(cypher, node_id=node.node_id, prev_id=prev_id)
+                i = i + 1
+            session.close()
+        
+        print(f"{i} nodes saved.")
+        
+        print("Start saving objects to Neo4j...")
+        
+        i = 0
+        with driver.session() as session:
+            for node in objects:               
+                node_json = json.loads(node.json())
+        
+                # Object is a Table, then the ????_ref_table object is created as a Section, and the table object is Chunk
+                if node.node_id[-1 * len(TABLE_REF_SUFFIX):] == TABLE_REF_SUFFIX:
+                    if node.next_node is not None:  # here is where actual table object is loaded
+                        next_node = node.next_node
+        
+                        obj_metadata = json.loads(str(next_node.json()))
+        
+                        cypher  = "MERGE (s:Section {key: $node_id})\n"
+                        cypher += "WITH s MERGE (c:Chunk {key: $table_id})\n"
+                        cypher += " FOREACH (ignoreMe IN CASE WHEN c.type IS NULL THEN [1] ELSE [] END |\n"
+                        cypher += "     SET c.hash = $hash, c.definition=$content, c.text=$table_summary, c.type=$type, c.start_idx=$start_idx, c.end_idx=$end_idx )\n"
+                        cypher += " WITH s, c\n"
+                        cypher += " MERGE (s) <-[:UNDER_SECTION]- (c)\n"
+                        cypher += " WITH s MATCH (d:Document {url_hash: $doc_id})\n"
+                        cypher += " MERGE (d)<-[:HAS_DOCUMENT]-(s);"
+        
+                        session.run(cypher, node_id=node.node_id, hash=next_node.hash, content=obj_metadata['metadata']['table_df'], type='TABLE'
+                                          , start_idx=node_json['start_char_idx'], end_idx=node_json['end_char_idx']
+                                          , doc_id=node.ref_doc_id, table_summary=obj_metadata['metadata']['table_summary'], table_id=next_node.node_id)
+                        
+                    if node.prev_node is not None:
+                        cypher  = "MATCH (c:Section {key: $node_id})\n"    # current node should exist
+                        cypher += "MERGE (p:Section {key: $prev_id})\n"    # previous node may not exist
+                        cypher += "MERGE (p)-[:NEXT]->(c);"
+        
+                        if node.prev_node.node_id[-1 * len(TABLE_ID_SUFFIX):] == TABLE_ID_SUFFIX:
+                            prev_id = node.prev_node.node_id + '_ref'
+                        else:
+                            prev_id = node.prev_node.node_id
+                        
+                        session.run(cypher, node_id=node.node_id, prev_id=prev_id)
+                i = i + 1
+            session.close()
+        
+        print("Start creating chunks for each TEXT Section...")
+        
+        with driver.session() as session:
+            cypher  = "MATCH (s:Section) WHERE s.type='TEXT' \n"
+            cypher += "WITH s CALL {\n"
+            cypher += "WITH s WITH s, split(s.text, '\n') AS para\n"
+            cypher += "WITH s, para, range(0, size(para)-1) AS iterator\n"
+            cypher += "UNWIND iterator AS i WITH s, trim(para[i]) AS chunk, i WHERE size(chunk) > 0\n"
+            cypher += "CREATE (c:Chunk {key: s.key + '_' + i}) SET c.type='TEXT', c.text = chunk, c.seq = i \n"
+            cypher += "CREATE (s) <-[:UNDER_SECTION]-(c) } IN TRANSACTIONS OF 500 ROWS ;"
+            
+            session.run(cypher)
+            
+            session.close()
+
+        print(f"{i} objects saved.")
+        
+        print("=================DONE====================")
+        
+        driver.close()
         
         return {"status": "success", "message": "PDF processed and data stored successfully"}
     except Exception as e:
