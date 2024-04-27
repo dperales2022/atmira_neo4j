9d2de785ccf2e139d12d5ae12cc0193b922fd8dc
dperales2022 <111503354+dperales2022@users.noreply.github.com>
2024-04-27 15:02:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ import requests
 from tempfile import NamedTemporaryFile
 from llama_parse import LlamaParse
 from llama_index.core.node_parser import MarkdownElementNodeParser
+from openai import OpenAI
 import nest_asyncio
 nest_asyncio.apply()
 
@@ -54,6 +55,41 @@ def download_pdf(url):
     with NamedTemporaryFile(delete=False, suffix='.pdf') as tmp_file:
         tmp_file.write(response.content)
         return tmp_file.name  # Return the file path of the downloaded PDF
+
+def get_embedding(client, text, model):
+    response = client.embeddings.create(
+                    input=text,
+                    model=model,
+                )
+    return response.data[0].embedding
+
+def LoadEmbedding(label, property):
+    driver = GraphDatabase.driver(NEO4J_URL, auth=(NEO4J_USER, NEO4J_PASSWORD), database=NEO4J_DATABASE)
+    openai_client = OpenAI (api_key = os.environ["OPENAI_API_KEY"])
+
+    with driver.session() as session:
+        # get chunks in document, together with their section titles
+        result = session.run(f"MATCH (ch:{label}) RETURN id(ch) AS id, ch.{property} AS text")
+        # call OpenAI embedding API to generate embeddings for each proporty of node
+        # for each node, update the embedding property
+        count = 0
+        for record in result:
+            id = record["id"]
+            text = record["text"]
+            
+            # For better performance, text can be batched
+            embedding = get_embedding(openai_client, text, EMBEDDING_MODEL)
+            
+            # key property of Embedding node differentiates different embeddings
+            cypher = "CREATE (e:Embedding) SET e.key=$key, e.value=$embedding, e.model=$model"
+            cypher = cypher + " WITH e MATCH (n) WHERE id(n) = $id CREATE (n) -[:HAS_EMBEDDING]-> (e)"
+            session.run(cypher,key=property, embedding=embedding, id=id, model=EMBEDDING_MODEL) 
+            count = count + 1
+
+        session.close()
+        
+        print("Processed " + str(count) + " " + label + " nodes for property @" + property + ".")
+        return count
 
 # Database session setup
 def get_neo4j_session():
@@ -224,6 +260,9 @@ async def process_pdf(request: ProcessRequest):
         print("=================DONE====================")
         
         driver.close()
+
+        # For smaller amount (<2000) of text data to embed
+        LoadEmbedding("Chunk", "text")
         
         return {"status": "success", "message": "PDF processed and data stored successfully"}
     except Exception as e:
